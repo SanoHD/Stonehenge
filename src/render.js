7ba016368b46b867cf1da2035b2115eb120ad086
CMinusMinus <sanohd@posteo.de>
2021-05-16 22:20:29 +0200

    Added translations

--- src/render.js
+++ src/render.js
@@ -56,6 +56,19 @@ function translate(val) {
 
 document.getElementById("welcome-title").innerHTML = translate("welcome_title");
 
+let textCreate = document.getElementsByClassName("welcome-choose-text")[0];
+let textEdit = document.getElementsByClassName("welcome-choose-text")[1];
+textCreate.innerHTML = translate("welcome_create");
+textEdit.innerHTML = translate("welcome_edit");
+textCreate.innerHTML = textCreate.innerHTML.replace("{ICON}", "<span class=\"welcome-choose-text-color\">➕</span>")
+textEdit.innerHTML = textEdit.innerHTML.replace("{ICON}", "<span class=\"welcome-choose-text-color\">✒</span>")
+
+let smallTextCreate = document.getElementsByClassName("welcome-choose-text-small")[0];
+let smallTextEdit = document.getElementsByClassName("welcome-choose-text-small")[1];
+smallTextCreate.innerHTML = translate("welcome_create_small");
+smallTextEdit.innerHTML = translate("welcome_edit_small");
+
+
 function toggleTextureSidebar(event, type, texturePath) {
 	let ts = document.getElementById("texture-sidebar");
 	let et = event.target;
@@ -441,6 +454,7 @@ function loadWindow() {
 
 			} else {
 				filePath = filePath + "\\my-resource-pack";
+
 				pack = {
 					"name": "my-resource-pack",
 					"description": "Enter a description here!",
